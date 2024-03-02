ba255d8e21871729790c3606fc6fd8c2c95f30a2
Hailey Pan <haileyrpan@gmail.com>
2024-03-02 11:40:43 -0500

    add borders fix

--- frontend/src/components/portfolio/ListItem.jsx
+++ frontend/src/components/portfolio/ListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ListItem = ({ children }) => {
   return (
-    <li className="py-2 last:border-b-0 text-start">
+    <li className="py-2 border-b last:border-b-0 border-gray-300 text-start">
       <div className="border-b border-gray-100">{children}</div>
     </li>
   );
