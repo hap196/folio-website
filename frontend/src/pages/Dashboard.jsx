565b7bc2bdaeef87031c6296f0a0d35be830f666
Hailey Pan <haileyrpan@gmail.com>
2024-03-02 08:05:38 -0500

    add tailwind

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/userContext";
+import "../index.css";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="bg-red-100">Dashboard</h1>
       {!!user && <h2>Hi, {user.name}!</h2>}
     </div>
   );
